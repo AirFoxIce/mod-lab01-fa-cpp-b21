c83518a2178a94183c2f4262504bb68c574e4b86
Ivan Vladimirov <vivalexy@ya.ru>
2025-03-22 16:50:49 +0300

    Написана faStr1()

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,8 +1,30 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <cctype>
+
 
 unsigned int faStr1(const char *str) {
-    return 0;
+    int count_words = 0;
+    bool has_number = false;
+    bool in_word = false;
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (isspace(str[i])) {
+            if (in_word && !has_number) {
+                count_words++;
+            }
+            in_word = false;
+            has_number = false;
+        } else {
+            if (isdigit(str[i])) has_number = true;
+            in_word = true;
+        }
+    }
+
+    if (in_word && !has_number) {
+        count_words++;
+    }
+    return count_words;
 }
 
 unsigned int faStr2(const char *str) {
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 // Copyright 2022 UNN-IASR
 #include "fun.h"
+#include <iostream>
+using namespace std;
+
 
 int main() {
+    const char* testStr = "qwe4  gftt e 555gr rrgr  grhh ";
+    int result = faStr1(testStr);
+    cout << "faStr1 result: " << result;
     return 0;
 }
